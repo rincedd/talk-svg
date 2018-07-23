3c6e4fa325d661af2483702ad128228324f2fb82
Gerd Zschaler <gerd@gzschaler.de>
2018-07-23 23:02:41 +0200

    Add title slide with poor man's searchlight effect

--- presentation/index.js
+++ presentation/index.js
@@ -4,10 +4,12 @@ import {
   CodePane, Deck, Fill, Heading, Image, Layout, Link, List, ListItem, Slide, Text,
 } from 'spectacle';
 import createTheme from 'spectacle/lib/themes/default';
+import background from '../assets/donnie-rosie-605394-unsplash.jpg';
+import { hideSearchLight, showSearchLight } from './searchlight';
 
 const images = {
   logo: require('../assets/tng.svg'),
-  svgSupport: require('../assets/svg-support.png'),
+  svgSupport: require('../assets/svg-support.png')
 };
 
 const theme = createTheme(
@@ -15,38 +17,59 @@ const theme = createTheme(
     primary: 'white',
     secondary: '#1F2022',
     tertiary: '#03A9FC',
-    quarternary: '#CECECE',
+    quarternary: '#CECECE'
   },
   {
     primary: {
       name: 'Open Sans',
       googleFont: true,
-      styles: ['400', '700'],
+      styles: ['400', '700']
     },
     secondary: {
-      name: 'Lato',
+      name: 'Luckiest Guy',
       googleFont: true,
-      styles: ['700'],
-    },
-  },
+      styles: ['400']
+    }
+  }
 );
 
 export default class Presentation extends Component {
   render() {
     return (
       <Deck
-        transition={['fade']} transitionDuration={500} theme={theme} contentWidth={1600}
+        transition={['fade']}
+        transitionDuration={500}
+        theme={theme}
+        contentWidth={1600}
         contentHeight={900}
       >
-        <Slide>
-          <Text>You should be using</Text>
-          <Heading size={1} caps textColor="secondary" textFont="secondary">
-            SVG in your web app
-          </Heading>
-          <Text margin="25px 0" bold textColor="tertiary">
-            Gerd Zschaler
+        <Slide transition={[() => ({})]} bgImage={background} onActive={() => showSearchLight()}>
+          <Text textSize="6rem" textColor="rgba(255, 255, 255, 0.85)" textFont="secondary">
+            You should be using
           </Text>
-          <Image src={images.logo} width="28%" />
+          <Text textSize="14rem" textColor="rgba(255, 255, 255, 0.85)" textFont="secondary">
+            S V G
+          </Text>
+          <Text textSize="6rem" textColor="rgba(255, 255, 255, 0.85)" textFont="secondary">
+            in your web app
+          </Text>
+        </Slide>
+        <Slide onActive={() => hideSearchLight()}>
+          <Text textSize="6rem" textFont="secondary">
+            You should be using
+          </Text>
+          <Text textSize="14rem" textFont="secondary">
+            S V G
+          </Text>
+          <Text textSize="6rem" textFont="secondary">
+            in your web app
+          </Text>
+          <div style={{ position: 'absolute', left: 0, right: 0, marginTop: '10rem' }}>
+            <Text bold textColor="tertiary" margin="0.25rem auto 2rem auto">
+              Gerd Zschaler
+            </Text>
+            <Image src={images.logo} width="16%" />
+          </div>
         </Slide>
         <Slide>
           <Heading size={2}>Scalable Vector Graphics</Heading>
@@ -67,11 +90,7 @@ export default class Presentation extends Component {
           <Heading size={2}>SVG is code</Heading>
           <Layout>
             <Fill>
-              <CodePane
-                lang="markup" theme="light"
-                source={require('raw-loader!./sample-svg.xml')}
-                textSize={18}
-              />
+              <CodePane lang="markup" theme="light" source={require('raw-loader!./sample-svg.xml')} textSize={18} />
             </Fill>
             <Fill>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
---
+++ presentation/searchlight.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const searchLightStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 10000
+};
+
+const radius = 15;
+const pathPoints = [[radius, 50], [20, radius], [140, 90 - radius], [160 - radius, 50], [140, radius], [20, 90 - radius]];
+const path = `M${pathPoints.map(p => p.join(' ')).join('L')}Z`;
+
+const SearchLight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 160 90"
+    style={searchLightStyle}
+  >
+    <defs>
+      <path id="spotlight-path" d={path} />
+      <mask id="spotlight-mask">
+        <rect x="0" y="0" width="160" height="90" fill="#fff" />
+        <circle id="spot" r={radius} fill="#000" style={{ transition: 'r .5s' }}>
+          <animateMotion dur="20s" repeatCount="indefinite">
+            <mpath xlinkHref="#spotlight-path" />
+          </animateMotion>
+        </circle>
+      </mask>
+    </defs>
+    <rect mask="url(#spotlight-mask)" x="0" y="0" width="160" height="90" fill="#000" />
+  </svg>
+);
+
+const SEARCH_LIGHT_ID = 'search-light';
+
+export const showSearchLight = () => {
+  const searchLight = document.createElement('div');
+  searchLight.setAttribute('id', SEARCH_LIGHT_ID);
+  document.querySelector('body').appendChild(searchLight);
+  ReactDOM.render(<SearchLight />, searchLight);
+};
+
+export const hideSearchLight = () => {
+  const spot = document.querySelector('#spot');
+
+  if (spot) {
+    spot.setAttribute('r', '100');
+    setTimeout(() => {
+      const searchLight = document.querySelector(`#${SEARCH_LIGHT_ID}`);
+      searchLight.parentNode.removeChild(searchLight);
+    }, 500);
+  }
+};
