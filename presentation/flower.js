6397c2e130149ac6376c80d5886104a787f846ce
Gerd Zschaler <gerd.zschaler@tngtech.com>
2018-08-17 00:08:38 +0200

    Use slider to control flower

--- presentation/flower.js
+++ presentation/flower.js
@@ -28,11 +28,18 @@ export class Flower extends Component {
       .map((_, i) => (i * 360) / numPetals);
     return (
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -40 100 80" style={{width: '20rem'}}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -40 100 80" style={{ width: '20rem' }}>
           {angles.map((a) => <Petal key={a} angle={a} />)}
         </svg>
-        <button onClick={() => this.setState({ numPetals: this.state.numPetals + 1 })}> + </button>
-        <button onClick={() => this.setState({ numPetals: Math.max(0, this.state.numPetals - 1) })}> - </button>
+        <input
+          name="numPetals"
+          type="range"
+          min="0"
+          max="30"
+          step="1"
+          defaultValue={3}
+          onChange={(e) => this.setState({ numPetals: Number(e.target.value) })}
+        />
       </div>
     );
   }
